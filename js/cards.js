ef11b579a6203e11604eea2c67bd66c219e2d755
Edouard Quennesson <edouardquennesson@macbook-air-de-edouard.home>
2023-07-21 11:31:21 +0200

    added ingredients dom, need to fix the issue with only one div.

--- js/cards.js
+++ js/cards.js
@@ -1,53 +1,60 @@
 import {recipes} from './recipes.js';
 
-function createCard() {
-    let recipeSrc, recipeHeading, recipeDescription, recipeIngredients, recipeTime, col = [], htmlDom;
+
+function setIngredientsDom(){
+    let divAllIngredients = [], divIngredient = [], divQuantityUnit = [];
 
     for (let i=0; i<recipes.length; i++){
-        recipeSrc = `/assets/photos plats/${recipes[i].image}`
-        recipeIngredients = recipes[i].ingredients;
-        recipeTime = recipes[i].time;
-        recipeHeading = recipes[i].name;
-        //p card text en dessous de R E C E T T E
-        recipeDescription = recipes[i].description;
-        // génère une card text en desous de I N G R E D I E N T S pour chaque ingrédients 
+        // génère un text en desous de I N G R E D I E N T S pour chaque ingrédients 
+        divAllIngredients[i] = document.createElement('div');
+        divAllIngredients[i].setAttribute('class', "card-text col-6 d-flex flex-column");
 
+        for (let j=0; j<recipes[i].ingredients.length; j++){
+
+            divIngredient = document.createElement('span');
+            divIngredient.setAttribute('class', "text-dark");
+            
+            divIngredient.innerText = recipes[i].ingredients[j].ingredient;
+
+            if (recipes[i].ingredients[j].quantity !== undefined){
+                divQuantityUnit = document.createElement('span');
+                divQuantityUnit.setAttribute('class', "text-secondary");
+                if(recipes[i].ingredients[j].unit !== undefined){                
+                    divQuantityUnit.innerText = recipes[i].ingredients[j].quantity + ' ' + recipes[i].ingredients[j].unit; 
+                }
+                else{
+                    divQuantityUnit.innerText = recipes[i].ingredients[j].quantity;
+                }
+                divAllIngredients[i].appendChild(divIngredient);
+                divAllIngredients[i].appendChild(divQuantityUnit);
+            }
+            else{
+                divAllIngredients[i].appendChild(divIngredient);
+            }
+        }
+    }
+    return divAllIngredients;
+}
+
+function createCard() {
+    let col = [], htmlDom;
+
+    for (let i=0; i<recipes.length; i++){  
         //dom
         htmlDom = 
         ` 
-            <div class="card position-relative rounded-4" style="width: 380px;">
-                <span class="card-time | bg-primary px-3 py-1 position-absolute end-5 top-3 rounded-pill" style="font-size: 12px;">${recipeTime} min</span>
-                <img src="${recipeSrc}" class="card-img-top rounded-top-4" alt="Photo recette">
+            <div class="card position-relative rounded-4 shadow" style="width: 380px;">
+                <span class="card-time | bg-primary px-3 py-1 position-absolute end-5 top-3 rounded-pill" style="font-size: 12px;">${recipes[i].time} min</span>
+                <img src="/assets/photos plats/${recipes[i].image}" class="card-img-top rounded-top-4" alt="Photo recette">
                 <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">${recipeHeading}</h5>
+                    <h5 class="card-title">${recipes[i].name}</h5>
                     <span class="text-secondary my-2">R E C E T T E</span>
-                    <p class="card-text">${recipeDescription}</p>
+                    <p class="card-text">${recipes[i].description}</p>
                 </div>
-                <div class="card-body d-flex flex-wrap ">
+                <div class="card-ingredients | card-body d-flex flex-wrap ">
                     <span class="text-secondary my-2 w-100">I N G R É D I E N T S</span>
-                    <div class="card-text col-6 d-flex flex-column">
-                    <span class="text-dark">Lait de coco</span>
-                    <span class="text-secondary">400ml</span>
-                    </div>
-                    <div class="card-text col-6 d-flex flex-column">
-                    <span class="text-dark">Jus de citron</span>
-                    <span class="text-secondary">2</span>
-                    </div>
-                    <div class="card-text col-6 d-flex flex-column">
-                    <span class="text-dark">Crème de coco</span>
-                    <span class="text-secondary">4 cuillères</span>
-                    </div>
-                    <div class="card-text col-6 d-flex flex-column">
-                    <span class="text-dark">Sucre</span>
-                    <span class="text-secondary">20g</span>
-                    </div>
-                    <div class="card-text col-6 d-flex flex-column">
-                    <span class="text-dark">Glaçons</span>
-                    <span class="text-secondary">2</span>
-                    </div>
                 </div>
             </div>           
-            
         `
         col[i] = document.createElement('div');
         col[i].setAttribute('class', "col d-flex justify-content-center")
@@ -58,17 +65,26 @@ function createCard() {
 }
 
 const rowCard = document.querySelector('.card-row');
-
 function init(){
+    let cardDomIngredients = setIngredientsDom();
     let cardDOM = createCard();
+    for (let i=0; i<recipes.length; i++){
+        // console.log(cardDomIngredients[i].innerHTML);
+        rowCard.append(cardDOM[i]);
+        // ingredient.appendChild(cardDomIngredients[i])
+    }
+    const cardIngredients = document.querySelectorAll('.card-ingredients');
 
     for (let i=0; i<recipes.length; i++){
-        rowCard.append(cardDOM[i]);
+        cardIngredients[i].append(cardDomIngredients[i]);
     }
+
     console.log(recipes[0]);
     const nbrRecettes = document.querySelectorAll('.card').length;
     const recettes = document.querySelector('.number-recipes');
     recettes.innerText = `${nbrRecettes} recettes`
+
 }
+
 
 init();
