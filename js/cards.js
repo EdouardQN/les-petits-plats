677e4e0a5f751d646392c0ccc13e7abb301206eb
Edouard Quennesson <edouardquennesson@macbook-air-de-edouard.home>
2023-07-25 14:21:18 +0200

    fixed the issue causing ingredients to be on only one div. Optimized code

--- js/cards.js
+++ js/cards.js
@@ -1,90 +1,121 @@
 import {recipes} from './recipes.js';
 
-
 function setIngredientsDom(){
-    let divAllIngredients = [], divIngredient = [], divQuantityUnit = [];
-
+    let divAllIngredients = [], spanIngredient, spanQuantityUnit, twoArrays = new Array(recipes.length);
     for (let i=0; i<recipes.length; i++){
-        // génère un text en desous de I N G R E D I E N T S pour chaque ingrédients 
+
+        twoArrays[i] = new Array(recipes[i].ingredients.length);
         divAllIngredients[i] = document.createElement('div');
-        divAllIngredients[i].setAttribute('class', "card-text col-6 d-flex flex-column");
 
         for (let j=0; j<recipes[i].ingredients.length; j++){
 
-            divIngredient = document.createElement('span');
-            divIngredient.setAttribute('class', "text-dark");
+            twoArrays[i][j] = document.createElement('div');
+            twoArrays[i][j].setAttribute('class', "card-text col-6 d-flex flex-column");
+            spanIngredient = document.createElement('span');
+            spanIngredient.setAttribute('class', "text-dark");
             
-            divIngredient.innerText = recipes[i].ingredients[j].ingredient;
+            spanIngredient.innerText = recipes[i].ingredients[j].ingredient;
 
             if (recipes[i].ingredients[j].quantity !== undefined){
-                divQuantityUnit = document.createElement('span');
-                divQuantityUnit.setAttribute('class', "text-secondary");
+                spanQuantityUnit = document.createElement('span');
+                spanQuantityUnit.setAttribute('class', "text-secondary");
                 if(recipes[i].ingredients[j].unit !== undefined){                
-                    divQuantityUnit.innerText = recipes[i].ingredients[j].quantity + ' ' + recipes[i].ingredients[j].unit; 
+                    spanQuantityUnit.innerText = recipes[i].ingredients[j].quantity + ' ' + recipes[i].ingredients[j].unit; 
                 }
                 else{
-                    divQuantityUnit.innerText = recipes[i].ingredients[j].quantity;
+                    spanQuantityUnit.innerText = recipes[i].ingredients[j].quantity;
                 }
-                divAllIngredients[i].appendChild(divIngredient);
-                divAllIngredients[i].appendChild(divQuantityUnit);
+                twoArrays[i][j].appendChild(spanIngredient);
+                twoArrays[i][j].appendChild(spanQuantityUnit);
             }
             else{
-                divAllIngredients[i].appendChild(divIngredient);
+                twoArrays[i][j].appendChild(spanIngredient);
             }
         }
     }
-    return divAllIngredients;
+    return twoArrays;
 }
 
-function createCard() {
-    let col = [], htmlDom;
+function setCardDom() {
+    let col = [], card, cardTime, cardImg, cardBody, cardTitle, cardRecipeTitle, cardRecipeText, cardIngredients, cardIngredientsText;
 
     for (let i=0; i<recipes.length; i++){  
-        //dom
-        htmlDom = 
-        ` 
-            <div class="card position-relative rounded-4 shadow" style="width: 380px;">
-                <span class="card-time | bg-primary px-3 py-1 position-absolute end-5 top-3 rounded-pill" style="font-size: 12px;">${recipes[i].time} min</span>
-                <img src="/assets/photos plats/${recipes[i].image}" class="card-img-top rounded-top-4" alt="Photo recette">
-                <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">${recipes[i].name}</h5>
-                    <span class="text-secondary my-2">R E C E T T E</span>
-                    <p class="card-text">${recipes[i].description}</p>
-                </div>
-                <div class="card-ingredients | card-body d-flex flex-wrap ">
-                    <span class="text-secondary my-2 w-100">I N G R É D I E N T S</span>
-                </div>
-            </div>           
-        `
+        card = document.createElement('div');
+        card.setAttribute('class', "card position-relative rounded-4 shadow");
+        card.setAttribute('style', "width: 380px;");
+
+        cardTime = document.createElement('span');
+        cardTime.setAttribute('class', "card-time | bg-primary px-3 py-1 position-absolute end-5 top-3 rounded-pill");
+        cardTime.setAttribute('style', "font-size: 12px;");
+        cardTime.innerText = `${recipes[i].time} min`;
+
+        cardImg = document.createElement('img');
+        cardImg.setAttribute('class', "card-img-top rounded-top-4");
+        cardImg.setAttribute('src', `/assets/photos plats/${recipes[i].image}`);
+        cardImg.setAttribute('alt', "Photo recette");
+
+        card.appendChild(cardTime);
+        card.appendChild(cardImg);
+
+        // Card Body | Recipe description
+        cardBody = document.createElement('div');
+        cardBody.setAttribute('class', "card-body d-flex flex-column");
+
+        cardTitle = document.createElement('h5');
+        cardTitle.setAttribute('class', "card-title");
+        cardTitle.innerText = `${recipes[i].name}`;
+
+        cardRecipeTitle = document.createElement('span');
+        cardRecipeTitle.setAttribute('class', "text-secondary my-2");
+        cardRecipeTitle.innerText = "R E C E T T E";
+
+        cardRecipeText = document.createElement('p');
+        cardRecipeText.setAttribute('class', "card-text");
+        cardRecipeText.innerText = `${recipes[i].description}`;
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardRecipeTitle);
+        cardBody.appendChild(cardRecipeText); 
+
+        // Card Ingredients
+        cardIngredients = document.createElement('div');
+        cardIngredients.setAttribute('class', "card-ingredients | card-body d-flex flex-wrap");
+
+        cardIngredientsText = document.createElement('span');
+        cardIngredientsText.setAttribute('class', "text-secondary my-2 w-100");
+        cardIngredientsText.innerText = "I N G R É D I E N T S";
+
+        cardIngredients.appendChild(cardIngredientsText);
+
+        card.appendChild(cardBody);
+        card.appendChild(cardIngredients);
+
         col[i] = document.createElement('div');
         col[i].setAttribute('class', "col d-flex justify-content-center")
-        col[i].innerHTML = htmlDom;
-        
+        col[i].appendChild(card);
     }
     return col;
 }
 
 const rowCard = document.querySelector('.card-row');
+
 function init(){
     let cardDomIngredients = setIngredientsDom();
-    let cardDOM = createCard();
+    let cardDOM = setCardDom();
     for (let i=0; i<recipes.length; i++){
-        // console.log(cardDomIngredients[i].innerHTML);
         rowCard.append(cardDOM[i]);
-        // ingredient.appendChild(cardDomIngredients[i])
     }
     const cardIngredients = document.querySelectorAll('.card-ingredients');
 
     for (let i=0; i<recipes.length; i++){
-        cardIngredients[i].append(cardDomIngredients[i]);
+        for (let j=0; j<recipes[i].ingredients.length; j++){
+            cardIngredients[i].append(cardDomIngredients[i][j]);
+        }
     }
-
     console.log(recipes[0]);
     const nbrRecettes = document.querySelectorAll('.card').length;
     const recettes = document.querySelector('.number-recipes');
     recettes.innerText = `${nbrRecettes} recettes`
-
 }
-
 
 init();
--- js/dropdowns.js
+++ js/dropdowns.js
@@ -4,20 +4,30 @@ const dropdownIngredients = document.querySelector('.dropdown-ingredients');
 const dropdownAppareil = document.querySelector('.dropdown-appareils');
 const dropdownUstentiles = document.querySelector('.dropdown-ustentiles');
 
-let tabIngredients = [], tabAppareils = [], tabUstentiles = [];
+function setCapitalLetter(word){
+    var firstLetter = word.slice(0,1);
+    var capitalized = word.replace(firstLetter,firstLetter.toUpperCase());
+    return capitalized;
+}
 
+// console.log(setCapitalLetter("brique"));
+
+//Sets arrays for dropdowns
+let tabIngredients = [], tabAppareils = [], tabUstentiles = [];
 for (let i=0; i<recipes.length; i++){
     for (let j=0; j<recipes[i].ingredients.length; j++){
-        tabIngredients[i] = recipes[i].ingredients[j].ingredient;
+        tabIngredients[i] = recipes[i].ingredients[j].ingredient.toLowerCase();
     }
-    tabAppareils[i] = recipes[i].appliance;
+    tabAppareils[i] = recipes[i].appliance.toLowerCase();
     for (let j=0; j<recipes[i].ustensils.length; j++){
-        tabUstentiles[i] = recipes[i].ustensils[j];
+        tabUstentiles[i] = recipes[i].ustensils[j].toLowerCase();
     }
+
 }
 
 function setDropdownElements(dropdownElement, elements){
     let domElementDropdown;
+
     const uniqueElement = elements.filter((el, index) => elements.indexOf(el) === index);
 
     for (let i=0; i<uniqueElement.length; i++){
